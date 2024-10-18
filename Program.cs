bb702d4ff0f963e855048386307aa3d71636091b
KissAdam <kissa@kkszki.hu>
2024-10-18 10:52:58 +0200

    Ready to use

--- Program.cs
+++ Program.cs
@@ -8,8 +8,55 @@ namespace Doga3
 {
     internal class Program
     {
+        static void homerseklet()
+        {
+            double[] temperatures = new double[7];
+
+            for (int i = 0; i < 7; i++)
+            {
+                Console.Write($"Kérlek, add meg a(z) {i + 1}. nap hőmérsékletét (°C): ");
+                temperatures[i] = Convert.ToDouble(Console.ReadLine());
+            }
+
+            double sum = 0;
+            double maxTemperature = temperatures[0];
+            double minTemperature = temperatures[0];
+            bool hasFrostDay = false;
+
+            for (int i = 0; i < 7; i++)
+            {
+                sum += temperatures[i];
+
+                if (temperatures[i] > maxTemperature)
+                {
+                    maxTemperature = temperatures[i];
+                }
+
+                if (temperatures[i] < minTemperature)
+                {
+                    minTemperature = temperatures[i];
+                }
+
+                if (temperatures[i] < 0)
+                {
+                    hasFrostDay = true;
+                }
+            }
+
+            double averageTemperature = sum / 7;
+
+            Console.WriteLine($"\nA hét átlaghőmérséklete: {averageTemperature:F2} °C");
+            Console.WriteLine($"A legmelegebb nap hőmérséklete: {maxTemperature} °C");
+            Console.WriteLine($"A leghidegebb nap hőmérséklete: {minTemperature} °C");
+
+            if (hasFrostDay)
+            {
+                Console.WriteLine("Fagypont alatti nap is volt!");
+            }
+        }
         static void Main(string[] args)
         {
+            homerseklet();
         }
     }
 }
